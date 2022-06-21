c4ac41d075ef24bcb6584cd30d753b3df24a954f
Brian Burwell <brianburwell11@gmail.com>
2022-06-21 11:14:29 -0400

    docs: add numpy-style docstrings

--- prettier_json.py
+++ prettier_json.py
@@ -3,11 +3,28 @@
 """
 
 __version__ = "0.1.0"
+__docformat__ = "numpy"
 
 
 def prettyjson(obj, indent=2, maxlinelength=80):
     """Renders JSON content with indentation and line splits/concatenations to fit maxlinelength.
-    Only dicts, lists and basic types are supported"""
+
+    Only dicts, lists and basic types are supported
+
+    Parameters
+    ----------
+    obj : dict, list, tuple, str
+        A python object to be converted to JSON
+    indent : int, optional
+        How many spaces to use as an indent before nested keys. Default is 2
+    maxlinelength : int, optional
+        How many characters to limit a single line to before wrapping to the next line. Default is 80 characters
+
+    Returns
+    -------
+    str
+        A representation of the JSON to be piped to a .json file
+    """
 
     items, _ = getsubitems(
         obj,
@@ -20,11 +37,32 @@ def prettyjson(obj, indent=2, maxlinelength=80):
 
 
 def getsubitems(obj, itemkey, islast, maxlinelength, indent):
+    """Get all of the items within an iterable object
+
+    Parameters
+    ----------
+    obj : any
+    itemkey : str
+        The key for the
+    islast : bool
+        Whether or not `obj` is the last item in the JSON object.
+    maxlinelength : int
+        How many characters to limit a single line to before wrapping to the next line. Default is 80 characters
+    indent : int
+        How many spaces to use as an indent before nested keys. Default is 2
+
+    Returns
+    -------
+    list[str]
+        All of the individual items in the obj
+    bool
+        Whether or not `obj` should be rendered on a single line
+    """
+
     items = []
     is_inline = (
         True  # at first, assume we can concatenate the inner tokens into one line
     )
-
     isdict = isinstance(obj, dict)
     islist = isinstance(obj, list)
     istuple = isinstance(obj, tuple)
@@ -154,6 +192,19 @@ def getsubitems(obj, itemkey, islast, maxlinelength, indent):
 
 
 def basictype2str(obj):
+    """Convert a python object to its string representation
+
+    Parameters
+    ----------
+    obj : any
+        Any python object. If the object is not a basic type (str, bool, None), it must have a `__str__()` method.
+
+    Returns
+    -------
+    str
+        A string representation of the given `obj`.
+    """
+
     if isinstance(obj, str):
         strobj = '"' + str(obj) + '"'
     elif isinstance(obj, bool):
@@ -166,7 +217,22 @@ def basictype2str(obj):
 
 
 def indentitems(items, indent, level):
-    """Recursively traverses the list of json lines, adds indentation based on the current depth"""
+    """Recursively traverses the list of json lines, adds indentation based on the current depth
+
+    Parameters
+    ----------
+    items : iterable
+    indent : int
+        How many spaces to use to differentiate between levels
+    level : int
+        How many `indent`s to place before the given item
+
+    Returns
+    -------
+    str
+        The `items` iterable as an indented JSON-style string
+    """
+
     res = ""
     indentstr = " " * (indent * level)
     for (i, item) in enumerate(items):
